ea4dcd5c6bd81cff65bace0cf22dc7546e928728
dan66677 <daniil.zamyshlyaev@mail.ru>
2024-06-26 21:17:16 +0300

    fgdg

---
+++ cmd/main/app.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func handler(w http.ResponseWriter, r *http.Request, params httprouter.Params)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", handler)
+}
